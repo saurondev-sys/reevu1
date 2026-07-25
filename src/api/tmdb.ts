ea07f1de31931e31606f912546fecfa5534bb7de
SaU262 <rahulchabbi@icloud.com>
2026-07-25 14:54:23 +0530

    Fix TMDB requests in production

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_BASE_URL = import.meta.env.PROD
+  ? "/api/tmdb"
+  : "https://api.themoviedb.org/3";
 const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
 const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN as string | undefined;
 
